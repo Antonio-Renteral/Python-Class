97064fca337fe814d05222d099db1f2d1fd3ed07
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 22:30:33 -0600

    Agregando comentarios a nuestro programa

--- src/Ejercicios/stop_codons.py
+++ src/Ejercicios/stop_codons.py
@@ -1,10 +1,16 @@
+#Solicitamos al usuario que ingrese una serie secuencias de DNA separadas por comas
 secuencias = input("Dame secuencias de DNA separadas por comas:\n").upper().split(",")
 
+#Definimos una lista de codones de paro
 stop_codons = ["TAA", "TAG", "TGA"]
 
+#Usamos una comprension de listas para identificar las secuencias que contienen al menos un codón de parada.
+#Para cada secuencia (seq) en "secuencias", verificamos si contiene algun codon de "stop_codons" usando any().
 secuencias_con_stop_codon = [seq for seq in secuencias if any(codon in seq for codon in stop_codons)]
 
+#Imprimimos un mensaje indicando que estas son las secuencias con codones de parada
 print("Secuencias que contienen un stop codon:")
+#Imprimimos la lista de secuencias que contienen al menos un codón de parada
 print(secuencias_con_stop_codon)
 
 #   Codigo de Hely:
