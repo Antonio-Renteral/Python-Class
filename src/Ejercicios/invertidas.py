4ad7308eb9681b43ba3bdba614431cdec1803050
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 22:27:34 -0600

    Agregando comentarios a nuestro programa

--- src/Ejercicios/invertidas.py
+++ src/Ejercicios/invertidas.py
@@ -1,5 +1,9 @@
+#Le pedimos una serie de secuencias al usuario que esten separadas por comas
 secuencias = input("Dame secuencias separadas por comas:\n").split(",")
 
+#utilizamos una comprension de listas para hacer la inversion
+#[::-1] invierte la secuencia, devolviendo los caracteres en orden inverso.
 secuencia_invertida = [secuencia.strip()[::-1] for secuencia in secuencias]
 
+#Imprimimos la secuencia que el usuario nos dio pero de forma invertida
 print(secuencia_invertida)
