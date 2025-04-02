a87431c57952d72567e4777490f39cfa4583dab0
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 22:19:24 -0600

    Agregando comentarios al codigo

--- src/Ejercicios/count-nucleotides.py
+++ src/Ejercicios/count-nucleotides.py
@@ -1,5 +1,9 @@
+#Pedimos al usuario una serie de secuencias separadas por comas
 secuencias = input("Dame secuencias separadas por comas:\n").upper().split(",")
 
+#Usamos una comprension de listas para contar la cantidad de nucleotidos que hay en cada secuencia
+#Para cada secuencia de la lista se genera una sublista
 conteo = [[f"A: {secuencia.count('A')}", f"T: {secuencia.count('T')}", f"G: {secuencia.count('G')}", f"C: {secuencia.count('C')}"] for secuencia in secuencias]
 
+#Imprimimos nuestro resultado para que el usuario pueda verlo
 print(conteo)
