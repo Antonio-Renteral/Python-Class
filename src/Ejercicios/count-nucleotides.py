a5ea3d4187ff197edd317924f43e05d4e2fe15ff
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 21:59:06 -0600

    Programa del ejercicio de contar la cantidad de nucleotidos

---
+++ src/Ejercicios/count-nucleotides.py
@@ -0,0 +1,5 @@
+secuencias = input("Dame secuencias separadas por comas:\n").upper().split(",")
+
+conteo = [[f"A: {secuencia.count('A')}", f"T: {secuencia.count('T')}", f"G: {secuencia.count('G')}", f"C: {secuencia.count('C')}"] for secuencia in secuencias]
+
+print(conteo)
