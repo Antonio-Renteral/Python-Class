449a7acf9a84606a4a21d9b60bdab54404b118ea
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 22:21:29 -0600

    Agregando comentarios a nuestro programa

--- src/Ejercicios/cut3.py
+++ src/Ejercicios/cut3.py
@@ -1,5 +1,10 @@
+#Solicitamos al usuario que ingrese una serie de secuencias separadas por comas
+#La función input captura la entrada del usuario en forma de texto
+#split(",") divide la cadena ingresada en una lista de secuencias, separadas por la coma
 secuencias = input("Dame secuencias separadas por comas:\n").split(",")
 
+#Uso de comprension de listas para obbtener los primeros tres caracteres
 codones_inicio = [secuencia[:3] for secuencia in secuencias]
 
+#Imprimimos el resultado obtenido
 print(codones_inicio)
