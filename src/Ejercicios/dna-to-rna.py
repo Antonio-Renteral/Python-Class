8d35927bc9221d3369f5fdaf49d1a70ec743a9a0
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 22:23:26 -0600

    Agregando comentarios a nuestro programa

--- src/Ejercicios/dna-to-rna.py
+++ src/Ejercicios/dna-to-rna.py
@@ -1,5 +1,9 @@
+#Solicitamos al usuario que nos de una serie de secuencias separadas por comas
 secuencias = input("Dame secuencias separadas por comas:\n").split(",")
 
+#utilizamos una comprension de listas
+#Para cada secuencia en la lista, replace("T", "U") reemplaza la base nitrogenada Timina (T) por Uracilo (U)
 secuencias_arn = [secuencia.replace("T","U") for secuencia in secuencias]
 
+#Imprime la nueva secuencia que ahora sera de RNA
 print(secuencias_arn)
