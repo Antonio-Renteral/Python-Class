b2d67ff4a6f94d4126370781c3a80d2817811edc
Antonio-Renteral <addielpr@lcg.unam.mx>
2025-04-01 22:13:43 -0600

    Agregando comentarios al programa

--- src/Ejercicios/count_seq.py
+++ src/Ejercicios/count_seq.py
@@ -1,7 +1,9 @@
 inputfile = "results/Ejercicios/dna_sequences.fa"
 
-with open(inputfile,"r") as infile:
+with open(inputfile,"r") as infile: #Con esto abrimos el archivo con el que vamos a trabajar
     lineas = infile.readlines()
 
+#Usamos comprension de listas para filtrar unicamente las lineas que comienzan con ">"
 lineas_filtradas = [linea for linea in lineas if linea.startswith(">")]
+#Imprimimos la cantidad de lineas filtradas
 print(len(lineas_filtradas))
